5b7bda9af8e7620bb19985a3414fcc9d08488777
cienceTM <83795793+cienceTM@users.noreply.github.com>
2022-11-12 23:50:47 -0600

    Create VaseAutofarm.lua

---
+++ src/VaseAutofarm.lua
@@ -0,0 +1,141 @@
+--[[
+made by cience
+]]--
+
+
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+
+local GUI = Mercury:Create{
+    Name = "Mercury",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+
+local Tab = GUI:Tab{
+	Name = "Main",
+	Icon = "rbxassetid://8569322835"
+}
+
+Tab:Toggle{
+	Name = "Ringify",
+	StartingState = false,
+	Description = "Teleport yourself in a ring around the map 3 times",
+	Callback = function(state)
+
+        if state == true then
+            
+            -- First ring
+
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-1940.111572265625, 2.6, -47.71851348876953))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-1775.236083984375, -97.64706463623047, -983.119873046875))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-1286.287109375, 2.6, -1605.92626953125))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-358.02557373046875, -97.64706463623047, -2015.4129638671875))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(525.0869140625, 2.6, -1953.5865478515625))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(1307.1048583984375, -97.64706463623047, -1497.2767333984375))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(1911.3172607421875, 2.6, -721.716552734375))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(2069.746337890625, -97.64706463623047, 56.74637222290039))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(1517.7098388671875, 2.6, 1380.430908203125))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(813.21240234375, -97.64706463623047, 1925.263916015625))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-71.55492401123047, 2.6, 2103.811767578125))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-965.5327758789062, -97.64706463623047, 1859.4847412109375))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-1695.259765625, 2.6, 1268.830322265625))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-2006.4298095703125, -97.64706463623047, 630.73828125))       
+
+        -- Middle ring
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-1341.953857421875, 2.6, -20.971141815185547))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-1155.523193359375, -97.64706463623047, -535.841064453125))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-755.875244140625, 2.6, -1066.5064697265625))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-45.93109893798828, -97.64706463623047, -1320.146728515625))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(471.362548828125, 2.6, -1112.181396484375))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(1272.812744140625, -97.64706463623047, -430.69853515625))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(1342.21142578125, 2.6, 8.024580001831055))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(1224.0899658203125, -97.64706463623047, 584.224609375))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(974.9697875976562, 2.6, 1047.1878662109375))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(396.16693115234375, -97.64706463623047, 1427.571044921875))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-474.228759765625, 2.6, 1389.925537109375))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-961.2122192382812, -97.64706463623047, 946.5496215820312))
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-1268.237548828125, 2.6, 424.73858642578125))
+            
+           
+
+    -- inner ring
+            
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-569.0584106445312, -97.64706463623047, 11.512777328491211))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-466.1272888183594, 2.6, -408.5321044921875))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-19.190258026123047, -97.64706463623047, -734.0167236328125))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(543.5986938476562, 2.6, -325.6645812988281))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(637.5612182617188, -97.64706463623047, -15.692267417907715))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(574.333251953125, 2.6, 447.5785827636719))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(317.5814208984375, -97.64706463623047, 853.6527099609375))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-250.84646606445312, 2.6, 961.3445434570312))
+        wait(1.5)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-638.9310302734375, -97.64706463623047, 473.1532287597656))
+
+
+    end 
+
+        
+        
+        if state == false then
+            
+        end 
+    end
+}
+
+Tab:Button{
+	Name = "Teleport To the nearest vase",
+	Description = nil,
+	Callback = function() 
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+        wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+        wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+        wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+        wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+        wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+        wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+	wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+	wait(0.1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Vases.Model.Vase.CFrame
+    end
+}
